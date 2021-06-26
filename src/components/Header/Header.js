dd5ed7ffc47dd32f638239352d0f586130febb1b
Samin1615088 <samin.shafkat@gmail.com>
2021-06-26 15:07:57 +0600

    done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <header className="relative">
-            <div className="absolute z-50 text-7xl left-1/2 font-lora">
+            <div className="absolute z-50 text-4xl transform -translate-x-1/2 -translate-y-1/2 md:text-7xl font-lora top-1/2 left-1/2">
                 {/* <span className="inline-block">Writing makes you a better version of yoy</span> */}
                 <span className="inline-block text-white">Blogs</span>
             </div>
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -3,41 +3,43 @@ import { Link } from 'react-router-dom';
 
 const Post = ({ img }) => {
     return (
-        <div className="overflow-hidden bg-blue-500 border-4 border-red-500 rounded-md shadow-md">
-            <img
-                className="object-cover object-center border-b-2 border-red-500"
-                src={img}
-                alt=""
-            />
-            <div className="flex flex-col content-center justify-center p-5">
-                <div className="flex flex-row justify-center font-semibold text-indigo-600 font-varelaRound">
-                    <span className="px-2">
-                        <Link className="a" to="/posts?cat=Music">
-                            Music
+        <Link to="/single">
+            <div className="overflow-hidden bg-blue-500 border-4 border-red-500 rounded-md shadow-md">
+                <img
+                    className="object-cover object-center border-b-2 border-red-500"
+                    src={img}
+                    alt=""
+                />
+                <div className="flex flex-col content-center justify-center p-5">
+                    <div className="flex flex-row justify-center font-semibold text-indigo-600 font-varelaRound">
+                        <span className="px-2">
+                            <Link className="a" to="/posts?cat=Music">
+                                Music
+                            </Link>
+                        </span>
+                        <span className="px-2">
+                            <Link className="a" to="/posts?cat=Music">
+                                Life
+                            </Link>
+                        </span>
+                    </div>
+                    <span className="mt-5 font-bold font-josefin">
+                        <Link to="/post/abc" className="text-2xl uppercase">
+                            this is title
                         </Link>
                     </span>
-                    <span className="px-2">
-                        <Link className="a" to="/posts?cat=Music">
-                            Life
-                        </Link>
-                    </span>
+                    <hr />
+                    <span className="text-gray-200 font-lora">1 hour ago</span>
                 </div>
-                <span className="mt-5 font-bold font-josefin">
-                    <Link to="/post/abc" className="text-2xl uppercase">
-                        this is title
-                    </Link>
-                </span>
-                <hr />
-                <span className="text-gray-200 font-lora">1 hour ago</span>
-            </div>
-            <p className="p-5 pt-0 overflow-hidden overflow-clip">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
-                officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
-                fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
-                atque, exercitationem quibusdam, reiciendis odio laboriosam?
+                <p className="p-5 pt-0 overflow-hidden overflow-clip">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+                    officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+                    fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+                    atque, exercitationem quibusdam, reiciendis odio laboriosam?
 
-            </p>
-        </div>
+                </p>
+            </div>
+        </Link>
     );
 };
 
